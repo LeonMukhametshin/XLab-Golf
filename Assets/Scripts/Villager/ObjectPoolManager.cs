5d6cd5b2e540a8d0f41400c37ca6ddd17a1003d5
Leon <leonmukhametshin2004@gmail.com>
2025-11-11 15:53:57 +0300

    Code refactoring

--- Assets/Scripts/Villager/ObjectPoolManager.cs
+++ Assets/Scripts/Villager/ObjectPoolManager.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 
 public class ObjectPoolManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] m_prefabs; 
+    public static ObjectPoolManager Instance { get; private set; }
+
+    [SerializeField] private GameObject[] m_itemPrefabs; 
     [SerializeField] private int m_poolSize = 4; 
 
-    private Queue<GameObject> _pool = new();
-    public static ObjectPoolManager Instance { get; private set; }
+    private Queue<GameObject> m_pool = new();
 
     private void Awake()
     {
@@ -26,14 +27,14 @@ public class ObjectPoolManager : MonoBehaviour
 
     private void ValidatePrefabsArray()
     {
-        if(m_prefabs is null || m_prefabs.Length == 0)
+        if(m_itemPrefabs is null || m_itemPrefabs.Length == 0)
         {
             Debug.LogWarning("Array is empty");
         }
 
-        for (int i = 0; i < m_prefabs.Length; i++) 
+        for (int i = 0; i < m_itemPrefabs.Length; i++) 
         {
-            if (m_prefabs[i] is null)
+            if (m_itemPrefabs[i] is null)
             {
                 Debug.LogWarning($"Array element ¹{i} is null");
             }
@@ -50,12 +51,12 @@ public class ObjectPoolManager : MonoBehaviour
 
     public GameObject GetObject(Vector3 position, Quaternion rotation)
     {
-        if (_pool.Count == 0)
+        if (m_pool.Count == 0)
         {
             CreateNewObject();
         }
 
-        GameObject obj = _pool.Dequeue();
+        GameObject obj = m_pool.Dequeue();
         obj.transform.SetPositionAndRotation(position, rotation);
         obj.SetActive(true);
 
@@ -65,14 +66,13 @@ public class ObjectPoolManager : MonoBehaviour
     public void ReturnObject(GameObject obj)
     {
         obj.SetActive(false);
-        _pool.Enqueue(obj);
+        m_pool.Enqueue(obj);
     }
 
     private void CreateNewObject()
     {
-        GameObject randomPrefab = m_prefabs[Random.Range(0, m_prefabs.Length)];
-        GameObject obj = Instantiate(randomPrefab);
+        GameObject obj = Instantiate(m_itemPrefabs[Random.Range(0, m_itemPrefabs.Length)]);
         obj.SetActive(false);
-        _pool.Enqueue(obj);
+        m_pool.Enqueue(obj);
     }
 }
